dc6d42a0a33d26292d6ea69ef862b2d8369ef471
Farhan Ahmed <joeylauntner@gmail.com>
2016-11-01 23:51:51 +0600

    v1
    Beginning changes to the files

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,66 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name" : "John Doe",
+    "role" : "Web Developer",
+    "Contacts" : {
+        "mobile":"630-333-6666",
+        "email":"john@example.com",
+        "github":"johndoe",
+        "twitter":"@johndoe",
+        "location":"San Francisco"
+    },
+    "welcomeMessage":"lorem ipsum dolor sit amet",
+    "skills":[
+        "awesomeness", "delivering things", "cryogenic sleep", "saving the universe"
+    ],
+    "biopic":"images/fry.jpg"
+}
+
+var education = {
+    "schools":[
+        {
+            "name": "North South University",
+            "city":"Dhaka",
+            "degree":"Honors",
+            "majors":["CSE"],
+            "dates":2016,
+            "url":"http://example.com"
+        },
+        {
+            "name": "eckerd college",
+            "city":"bahadur",
+            "degree":"masters",
+            "majors":["CSE"],
+            "dates":2003,
+            "url":"http://example.com"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title":"JavaScript Crash Course",
+            "school":"Udacity",
+            "dates":2014,
+            "url":"http://www.udacity.com/course/ud804"
+        }
+    ]
+}
+
+var work = {
+    "jobs": [
+        {
+            "employer":"Pizza Palace",
+            "title":"Delivery Boy",
+            "dates":"2651 - december 335965",
+            "description":"Abdul Kuddus akjon boyati j ajaira chillachilli kore nd stuff so ore always avoid kora bhalo r dim kinte poysha lage onk.Dim khaite onk moja kintu"
+        },
+        {
+            "employer":"Planet Express",
+            "title":"Delivery Boy",
+            "dates":"January 3000-Future",
+            "description":"Abdul Kuddus akjon boyati j ajaira chillachilli kore nd stuff so ore always avoid kora bhalo r dim kinte poysha lage onk.Dim khaite onk moja kintu"
+        }
+    ]
+}
+
+var projects = {
+
+}
